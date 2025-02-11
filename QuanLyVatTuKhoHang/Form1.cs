1f7a7e07f5bb4cb6c39302743c79f27146c7070c
fillbertRiko <147192155+fillbertRiko@users.noreply.github.com>
2025-02-11 23:57:57 +0700

    Upload project
    Upload project into git

---
+++ QuanLyVatTuKhoHang/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyVatTuKhoHang
+{
+    public partial class Form1 : Form
+    {
+        string connectstring = @"Data Source = DESKTOP-P7SFE1O\DDHUY; Initial Catalog=SalesManagement; Intergrated Security=True";
+        SqlConnection con;
+        SqlDataAdapter adt;
+        SqlCommand cmd;
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
