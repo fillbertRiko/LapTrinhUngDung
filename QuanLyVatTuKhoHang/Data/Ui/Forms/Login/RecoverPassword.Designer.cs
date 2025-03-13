d8c0fd2a044f13e767f1b87aa6edd2aed72c506a
Dang_Duong_Huy <147192155+fillbertRiko@users.noreply.github.com>
2025-03-13 15:33:27 +0700

    Bug4

--- QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
+++ QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
@@ -37,16 +37,22 @@
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtRePassword = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
-            this.btnQuenmk = new Guna.UI2.WinForms.Guna2Button();
+            this.btnRecover = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.pictureBox1.Location = new System.Drawing.Point(278, 10);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(284, 170);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+=======
+            this.pictureBox1.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 111);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -54,7 +60,11 @@
             // 
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.lblUsername.Location = new System.Drawing.Point(141, 224);
+=======
+            this.lblUsername.Location = new System.Drawing.Point(109, 164);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(131, 28);
             this.lblUsername.TabIndex = 1;
@@ -64,7 +74,11 @@
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.lblEmail.Location = new System.Drawing.Point(141, 382);
+=======
+            this.lblEmail.Location = new System.Drawing.Point(109, 284);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(63, 28);
             this.lblEmail.TabIndex = 2;
@@ -74,7 +88,11 @@
             // 
             this.lblRePassword.AutoSize = true;
             this.lblRePassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.lblRePassword.Location = new System.Drawing.Point(141, 330);
+=======
+            this.lblRePassword.Location = new System.Drawing.Point(109, 244);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.lblRePassword.Name = "lblRePassword";
             this.lblRePassword.Size = new System.Drawing.Size(175, 28);
             this.lblRePassword.TabIndex = 3;
@@ -84,7 +102,11 @@
             // 
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.lblPassword.Location = new System.Drawing.Point(141, 277);
+=======
+            this.lblPassword.Location = new System.Drawing.Point(109, 204);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(98, 28);
             this.lblPassword.TabIndex = 4;
@@ -92,38 +114,63 @@
             // 
             // txtName
             // 
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtName.Location = new System.Drawing.Point(370, 226);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(264, 26);
+=======
+            this.txtName.Location = new System.Drawing.Point(301, 170);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(264, 22);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.txtName.TabIndex = 5;
             // 
             // txtEmail
             // 
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtEmail.Location = new System.Drawing.Point(370, 385);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(264, 26);
+=======
+            this.txtEmail.Location = new System.Drawing.Point(301, 285);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(264, 22);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.txtEmail.TabIndex = 6;
             // 
             // txtRePassword
             // 
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.txtRePassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtRePassword.Location = new System.Drawing.Point(370, 332);
             this.txtRePassword.Name = "txtRePassword";
             this.txtRePassword.Size = new System.Drawing.Size(264, 26);
+=======
+            this.txtRePassword.Location = new System.Drawing.Point(301, 247);
+            this.txtRePassword.Name = "txtRePassword";
+            this.txtRePassword.Size = new System.Drawing.Size(264, 22);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.txtRePassword.TabIndex = 7;
             // 
             // txtPassword
             // 
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.txtPassword.Location = new System.Drawing.Point(370, 279);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(264, 26);
+=======
+            this.txtPassword.Location = new System.Drawing.Point(301, 208);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(264, 22);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.txtPassword.TabIndex = 8;
             // 
-            // btnQuenmk
+            // btnRecover
             // 
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.btnQuenmk.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnQuenmk.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnQuenmk.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -137,14 +184,30 @@
             this.btnQuenmk.Size = new System.Drawing.Size(218, 50);
             this.btnQuenmk.TabIndex = 9;
             this.btnQuenmk.Text = "Quên mật khẩu ";
+=======
+            this.btnRecover.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnRecover.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecover.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnRecover.Location = new System.Drawing.Point(303, 371);
+            this.btnRecover.Name = "btnRecover";
+            this.btnRecover.Size = new System.Drawing.Size(177, 53);
+            this.btnRecover.TabIndex = 9;
+            this.btnRecover.Text = "Lấy Lại Mật Khẩu";
+            this.btnRecover.UseVisualStyleBackColor = false;
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             // 
             // FormRecover
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
             this.ClientSize = new System.Drawing.Size(892, 511);
             this.Controls.Add(this.btnQuenmk);
             this.Controls.Add(this.pictureBox1);
+=======
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRecover);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtRePassword);
             this.Controls.Add(this.txtEmail);
@@ -153,6 +216,10 @@
             this.Controls.Add(this.lblRePassword);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.lblUsername);
+<<<<<<< HEAD:QuanLyVatTuKhoHang/Data/Ui/Forms/Login/RecoverPassword.Designer.cs
+=======
+            this.Controls.Add(this.pictureBox1);
+>>>>>>> parent of e104097 (update: form):QuanLyVatTuKhoHang/Auth/.Designer.cs
             this.Name = "FormRecover";
             this.Text = "Lấy lại mật khẩu";
             this.Load += new System.EventHandler(this.FormRecover_Load);
@@ -173,6 +240,6 @@
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.TextBox txtRePassword;
         private System.Windows.Forms.TextBox txtPassword;
-        private Guna.UI2.WinForms.Guna2Button btnQuenmk;
+        private System.Windows.Forms.Button btnRecover;
     }
 }
